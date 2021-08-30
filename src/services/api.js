2c3dcc6e3bb853c3b13cc68770a2a938f5f52117
stemili <stefanmilikic999@gmail.com>
2021-08-30 19:55:20 +0200

    update baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'https://crudcrud.com/api/6ca39aaf959a4021bab584a521dd7530';
+const baseURL = "https://crudcrud.com/api/4b16a97834304f4d8f5d715c10434633";
 
 const axiosInstance = axios.create({
-	baseURL: baseURL,
-	timeout: 5000,
-	headers: {
-		accept: 'application/json',
-	}, 
+  baseURL: baseURL,
+  timeout: 5000,
+  headers: {
+    accept: "application/json",
+  },
 });
 
-export const callApi = ({
+export const callApi = ({ url, method, data }) =>
+  axiosInstance({
     url,
-    method,
+    method: method || "GET",
     data,
-  }) =>
-    axiosInstance({
-      url,
-      method: method || "GET",
-      data,
-    }).then(response => response.data);
+  }).then(response => response.data);
